c3435298210d3a4f93086900e8c145f4d868543c
Enning94 <enningfestus@gmail.com>
2023-10-31 15:58:44 +0000

    create project section

---
+++ src/components/projectData.js
@@ -0,0 +1,42 @@
+// projectData.js
+
+const projectData = [
+  {
+    id: 'NextView',
+    Name: 'Next View',
+    description: 'This project utilizes \'tvmaze\' API to fetch data and show cards, along with another API to retrieve and update likes and comments.',
+    date: 'March 2023',
+    collaborativeStatus: 'Collaborated',
+    skills: ['Javascript', 'HTML/CSS', 'Jest'],
+    projectURL: 'https://example.com/nextview',
+    githubRepository: 'https://github.com/yourusername/nextview',
+    projectImage: 'images/project-img/nextview.png',
+    technologiesUsed: ['React', 'Node.js', 'Express', 'MongoDB'],
+  },
+  {
+    id: 'CryptoPlace',
+    Name: 'Crypto Place',
+    description: 'Crypto-Place is a SPA, in this project we are fetching the cryptocurrency data from the API, user can see the live details and search for a specific coin.',
+    date: 'June 2023',
+    collaborativeStatus: 'Collaborated',
+    skills: ['React/Redux', 'JavaScript', 'Jest'],
+    projectURL: 'https://example.com/cryptoplace',
+    githubRepository: 'https://github.com/yourusername/cryptoplace',
+    projectImage: 'images/project-img/cryptoplace.png',
+    technologiesUsed: ['React', 'Redux', 'CryptoAPI'],
+  },
+  {
+    id: 'Leaderboard',
+    Name: 'Leader Board',
+    description: 'The Leaderboard project is a dynamic and user-friendly web application created using HTML, CSS, and JavaScript, with the added functionality of API integration. It serves as a versatile platform for ranking and showcasing user scores',
+    date: 'November 2022',
+    collaborativeStatus: 'Solo Project',
+    skills: ['React/Redux', 'JavaScript', 'Jest'],
+    projectURL: 'https://enning94.github.io/Leaderboard-API/dist/',
+    githubRepository: 'https://github.com/Enning94/Leaderboard-API',
+    projectImage: 'images/project-img/cryptoplace.png',
+    technologiesUsed: ['HTML', 'CSS', 'JavaScript', 'API Integration'],
+  },
+];
+
+export default projectData;
