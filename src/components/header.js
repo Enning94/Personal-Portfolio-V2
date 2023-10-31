a47e964f23de39b9ef97b3c2c910518ec792cdb8
Enning94 <enningfestus@gmail.com>
2023-10-31 15:56:53 +0000

    modify page header section

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <section className="headline-vh">
-        <div className="d-flex custom-navbar-bg  ms-style p-3 headline">
+        <div className="d-flex custom-navbar-bg  ms-style headline">
           <div className="d-flex sm-icon1">
             <div className="sm-title-wrapper">
               <h4 className="fs-6 sm-title">FOLLOW ME ON: </h4>
@@ -29,16 +29,28 @@ function Home() {
             </ul>
           </div>
           <div className="headline-text">
-            <h5 className="welcome-bg text-center p-2">Welcome! I&apos;m</h5>
-            <h2 className="fs-1">Festus</h2>
-            <h4>Software Developer</h4>
-            <p className="p-style">
-              {' '}
-              I can help you build a product , feature or website Look through
-              some of my work and experience! If you like what you see and have a
-              project you need coded, don&apos;t hestiate to contact me.
-              {' '}
-            </p>
+
+            <div>
+              <h4 className="welcome-bg">Welcome! I&apos;M</h4>
+            </div>
+            <h1 className="primary-text">
+              Festus Enning
+            </h1>
+            <div>
+              <h5>A Software Developer</h5>
+              <p className="p-style">
+                {' '}
+                I can help you build a product , feature or website.
+                <br />
+                Look through some of my work and experience!
+                <br />
+                If you like what you see and have a
+                project you need coded,
+                <br />
+                don&apos;t hestiate to contact me.
+                {' '}
+              </p>
+            </div>
           </div>
           <div className="space-container">
             <span />
